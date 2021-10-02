7109da12fe5d0cc6e3262270d1daae793605a793
alpha-ajax <91777892+alpha-ajax@users.noreply.github.com>
2021-10-02 13:42:02 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+new hoverEffect({
+  parent: document.querySelector(".distortion"),
+  intensity: 0.2,
+  image1: "./unsplash1.jpg",
+  image2: "./unsplash2.jpg",
+  //   default direction
+  //   angle: Math.PI/4,
+  angle: Math.PI / 8,
+  // displacementImage: "./ramen.png",
+  displacementImage: "./heightMap.png",
+});
