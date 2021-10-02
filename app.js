97b5d2ea7627144fb1261d75437e923d7ea51aee
Lakshya-productions <72144526+Lakshya-productions@users.noreply.github.com>
2021-10-02 13:59:07 +0530

    th (#2)
    th

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ new hoverEffect({
   intensity: 0.2,
   image1: "./unsplash1.jpg",
   image2: "./unsplash2.jpg",
-  //   default direction
-  //   angle: Math.PI/4,
   angle: Math.PI / 8,
-  // displacementImage: "./ramen.png",
-  displacementImage: "./heightMap.png",
+  displacementImage: "./ramen.png",
 });
